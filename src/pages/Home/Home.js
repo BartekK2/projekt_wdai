a8cd365276a206ff74ee1ed0263676fb348fe73c
Bartek Król <barteksp82@gmail.com>
2025-12-23 15:56:53 +0100

    wariant dla zalogowanych homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,26 +3,37 @@ import { DotLottiePlayer } from '@dotlottie/react-player';
 import '@dotlottie/react-player/dist/index.css';
 import './Home.css'; // import pliku CSS
 import { Link } from "react-router-dom";
+import {getCurrentUser, isLoggedIn} from "../../API/AUTH";
 
 /*
 TODO:
-- Gdy gotowe będzie przechowywanie sesji zalogowanego uzytkownika
-trzeba będzie dodać wariant dla zalogowanych bez info o logowaniu się
-a z info żeby od razu przejść do zakupów
 - może trzeba by było pogrzebać w animacji żeby zmienić kolor bluzki na inny
 bo się zlewa z tłem
 */
+
+
 
 function Home() {
   return (
     <div className="home-container">
       <div className="home-left">
         <Typography variant="h3" component="h1" className="home-title">
-          Witaj w naszej aplikacji!
+          Witaj {isLoggedIn && 'z powrotem'} w naszej aplikacji!
         </Typography>
         <Typography variant="h6" className="home-subtitle">
-          Zaloguj się, aby rozpocząć, lub zarejestruj nowe konto
+          {isLoggedIn?
+          'Miłych zakupów!'
+          :
+          'Zaloguj się, aby rozpocząć, lub zarejestruj nowe konto'
+          }
         </Typography>
+        {isLoggedIn ?
+        <div className="home-buttons">
+          <Button variant="contained" color="primary" size="large" component={Link} to="/login?action=login">
+            Nasze produkty
+          </Button>
+        </div>
+        :
         <div className="home-buttons">
           <Button variant="contained" color="primary" size="large" component={Link} to="/login?action=login">
             Zaloguj się
@@ -31,6 +42,8 @@ function Home() {
             Zarejestruj się
           </Button>
         </div>
+        }
+        
       </div>
       <div className="home-right">
         <DotLottiePlayer
